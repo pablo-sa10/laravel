e884b54117a84b7077a5fa6fc47a5db4d90cce33
pablo-sa10 <pablomoura2016@gmail.com>
2024-10-10 09:37:00 -0300

    gerando uma view e controller

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request){
+        
+        $series = [
+            'twd',
+            'got',
+            'dark'
+        ];
+
+        return view('listar-series', [
+            'series' => $series
+        ]);
+    }
+}
