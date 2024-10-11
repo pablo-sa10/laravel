0ed8db8fa7ae703161a44005fad91454fa56c16c
pablo-sa10 <pablomoura2016@gmail.com>
2024-10-11 11:01:10 -0300

    arrumei as rotas novamente pq sumiu apos fazer o migration, pelo motivo q nao sei

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout tittle="Nova Série">
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" class="form-control" id="nome" name="nome">        
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::get('/series/salvar', [SeriesController::class, 'index']);
 
 require __DIR__.'/auth.php';
